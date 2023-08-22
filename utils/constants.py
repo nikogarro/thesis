bacdd79c21b8985bedde52518e0a8b307ae84472
nikogarro <117863158+nikogarro@users.noreply.github.com>
2023-08-22 15:26:13 +0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,6 @@
 GOOD_CLASS_FOLDER = "good"
 DATASET_SETS = ["train", "test"]
 IMG_FORMAT = ".png"
+# μπορουμε να το μεταβαλουμε αναλογα με το μοντελο που επιλέγουμε
 INPUT_IMG_SIZE = (224, 224)
 NEG_CLASS = 1
